62a137fdd91e5284fbc9248faa01fd6f707c40af
Mauro-coder555 <mauopereyra96@gmail.com>
2023-11-23 18:22:27 -0300

    adding ETL

---
+++ ETL/utils.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+def data_overview(df):
+    '''
+    Analyzes the DataFrame and provides detailed insights into data types, null values, and general statistics.
+    
+    :param df: pandas DataFrame to be analyzed.
+    :return: DataFrame containing information on data types, null percentages, quantities, and general statistics.
+    '''
+    mi_dict = {"Column": [], "dType": [], "No_Null_%": [], "No_Null_Qty": [], "Null_%": [], "Null_Qty": []}
+    
+    duplicated_rows = df[df.duplicated()]
+    count_duplicated_rows = len(duplicated_rows)
+
+    for column in df.columns:
+        no_null_porcent = (df[column].count() / len(df)) * 100
+        mi_dict["Column"].append(column)
+        mi_dict["dType"].append(df[column].apply(type).unique())
+        mi_dict["No_Null_%"].append(round(no_null_porcent, 2))
+        mi_dict["No_Null_Qty"].append(df[column].count())
+        mi_dict["Null_%"].append(round(100 - no_null_porcent, 2))
+        mi_dict['Null_Qty'].append(df[column].isnull().sum())
+
+    overview = pd.DataFrame(mi_dict)
+    
+    print("\nTotal rows: ", len(df))
+    print("\nTotal full null rows: ", df.isna().all(axis=1).sum())
+    print("\nTotal duplicated rows:", count_duplicated_rows)
+    
+    return overview
